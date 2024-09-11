9e7c8a63562dce9c44f66ff0ab14ecceb9eb3c3f
Joao Vitor <jvcr1501@gmail.com>
2024-09-11 12:42:22 -0300

    fundamentos 005

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const start = () => {
-    let count = 1;
-    while(count <= 10) {
-        console.log(count);
-        count++;
+    while(true) {
+        let opcao = "sair";
+        switch(opcao) {
+            case "cadastrar":
+                console.log("vamos cadastrar");
+                break;
+            case "listar":
+                console.log("vamos listar");
+                break;
+            case "sair":
+                return;               
+        }
     }
     
     
