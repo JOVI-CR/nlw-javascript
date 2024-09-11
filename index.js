f8b6ffb231c86699a8c7f4eac7982a44803528f0
Joao Vitor <jvcr1501@gmail.com>
2024-09-11 12:27:58 -0300

    fundamentos 004

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-let meta = {
-    value: 'ler um livro por mês',
-    checked: true,
+const start = () => {
+    let count = 1;
+    while(count <= 10) {
+        console.log(count);
+        count++;
+    }
+    
+    
 }
 
-let metas = [
-    meta,
-    {
-        value: "caminhar 20 minutos todos os dias",
-        checked: false
-    }
-]
-
-console.log(metas[1].value);
+start();
