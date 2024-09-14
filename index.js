7ba37cf1361d8ad0285430271f6a3ce18399a449
Joao Vitor <jvcr1501@gmail.com>
2024-09-14 14:04:23 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -57,8 +57,24 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas: " + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return !meta.checked
+    })
+
+    if(abertas.length == 0) {
+        console.log("Não existem metas abertas!");
+        return;
+    }
+
+    await select ({
+        message: "Metas abertas: " + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -81,6 +97,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -97,6 +117,9 @@ const start = async () => {
                 break;
             case "realizadas":
                 await metasRealizadas()
+                break;
+            case "abertas":
+                await metasAbertas()
                 break;
             case "sair":
                 console.log("Até a próxima!");               
--- todo.md
+++ todo.md
@@ -1,7 +1,7 @@
 - [x]Criar metas
 - [x]Listar metas
     - [x] Metas realizadas 
-    - Metas abertas
+    - [x] Metas abertas
 - [x]Marcar/Desmarcar metas realizadas
 - Remover metas
 - Sistema de mensagens
